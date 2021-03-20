20daef4023869dc5c41897acc4da3675f66fec50
alphasabawu <alphasabawu@gmail.com>
2021-03-20 23:02:34 +0200

    modified allowed hosts

--- todo_drf/settings.py
+++ todo_drf/settings.py
@@ -9,12 +9,11 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-import  os
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -25,8 +24,7 @@ SECRET_KEY = '%23jz81$t8%(gni-^yn9m96-jo4*qd1x(n4q5c@mxw@h%hhl5r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['student-app-todo-api.herokuapp.com', '127.0.0.1']
-
+ALLOWED_HOSTS = ['student-app-todo-api.herokuapp.com', 'alphasabawu-crm1.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
@@ -75,17 +73,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'todo_drf.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME':  str(BASE_DIR / 'db.sqlite3'),
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -105,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -118,7 +113,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
